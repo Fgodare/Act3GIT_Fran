362fa8cd7630c3f976b99972416424a62de1c59e
Fran <fgodare0804@g.educaand.es>
2024-11-27 22:00:42 +0100

    Añadido tercer número en rama2

--- Act3GIT_Fran/src/Act3GIT_Fran/Act3GIT_Fran.java
+++ Act3GIT_Fran/src/Act3GIT_Fran/Act3GIT_Fran.java
@@ -1,16 +1,37 @@
 package Act3GIT_Fran;
 
-import java.util.Random;
+import java.util.*;
 
 public class Act3GIT_Fran {
 
 	public static void main(String[] args) {
-		
-		Random rand = new Random();
-		for (int i = 0; i < 20; i++) {
-		    System.out.println(rand.nextInt(10) + 1);  
-		}
+		Scanner sc = new Scanner(System.in);
 
+        System.out.print("Introduce el primer número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Introduce el segundo número: ");
+        int num2 = sc.nextInt();
+        
+        System.out.print("Introduce un tercer número: ");
+        int num3 = sc.nextInt();
+        
+        if (num1 == num2) {
+            System.out.println("Los números deben ser distintos.");
+            return;
+        }
+
+        int menor = Math.min(num1, num2);
+        int mayor = Math.max(num1, num2);
+
+        for (int i = menor; i <= mayor; i += 7) {
+            System.out.println(i);
+            if (i == num3) {
+                System.out.println("El tercer número está en el rango.");
+        }
+        
+     
 	}
 
+  }
 }
