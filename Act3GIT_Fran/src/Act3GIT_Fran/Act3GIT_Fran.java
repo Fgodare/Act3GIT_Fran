2e3c45f596625db257d6bcddc5ae6de00cbc25fa
Fran <fgodare0804@g.educaand.es>
2024-11-27 22:05:14 +0100

    Actualizo números random

--- Act3GIT_Fran/src/Act3GIT_Fran/Act3GIT_Fran.java
+++ Act3GIT_Fran/src/Act3GIT_Fran/Act3GIT_Fran.java
@@ -7,7 +7,7 @@ public class Act3GIT_Fran {
 	public static void main(String[] args) {
 		
 		Random rand = new Random();
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 30; i++) {
 		    System.out.println(rand.nextInt(10) + 1);  
 		}
 
