8ea6a80a74ad2a60dbd6ab617011bc9544757d41
Fran <fgodare0804@g.educaand.es>
2024-11-27 20:31:39 +0100

    Añado numeros random

---
+++ Act3GIT_Fran/src/Act3GIT_Fran/Act3GIT_Fran.java
@@ -0,0 +1,16 @@
+package Act3GIT_Fran;
+
+import java.util.Random;
+
+public class Act3GIT_Fran {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		for (int i = 0; i < 20; i++) {
+		    System.out.println(rand.nextInt(10) + 1);  
+		}
+
+	}
+
+}
