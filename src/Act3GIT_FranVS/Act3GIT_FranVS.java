bd83d81ffd2aa1451fbcdb3c71584caca0bf8a0f
Fran <fgodare0804@g.educaand.es>
2024-11-28 09:30:06 +0100

    Añado num3

--- src/Act3GIT_FranVS/Act3GIT_FranVS.java
+++ src/Act3GIT_FranVS/Act3GIT_FranVS.java
@@ -7,12 +7,21 @@ public class Act3GIT_FranVS {
 		
 		Scanner sc = new Scanner(System.in);
 
+<<<<<<< HEAD
         System.out.println("Ingrese dos números distintos:");
+=======
+        System.out.println("Introduzca tres números distintos:");
+>>>>>>> 25dd941 (Añado num3)
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
 
         if (num1 == num2) {
+<<<<<<< HEAD
             System.out.println("Los números deben ser distintos.");
+=======
+            System.out.println("Los números deben ser diferentes.");
+>>>>>>> 25dd941 (Añado num3)
             return;
         }
 
@@ -21,6 +30,9 @@ public class Act3GIT_FranVS {
 
         for (int i = start; i <= end; i += 7) {
             System.out.println(i);
+			if(i == num3){
+				System.out.println("El tercer número está en el rango.");
+			}
         }
 
 	}
